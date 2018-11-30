c85eec4c40be3a83977ba3929cfa6220f1e675a0
mcdonam7 <mcdonam7@tcd.ie>
2018-11-30 14:14:33 +0000

    beginning of interogation testing on console

--- firstfile.js
+++ firstfile.js
@@ -18,6 +18,17 @@ github.repos.getAll(
     var sizeArray = new Array(res.data.length)
     var languageArray = new Array(res.data.length)
     var commitsArray = new Array(res.data.length)
-}
+    console.log("\n***************************************")
+    console.log('\x1b[33m%s\x1b[0m', ("      Repositories, Size, Language       "))
+    console.log("***************************************")
+    for (var x=0; x<res.data.length; x++){
+      console.log((x+1)+ ". " + (res.data[x].name) + ", Size: " + (res.data[x].size) + "kb, Language: " + (res.data[x].language))
+      repoArray.push(res.data[x].name)
+      sizeArray[x] = res.data[x].size
+    }
+    console.log("Number of repositories: " + (res.data.length))
+  }
+)
+
 
 );
